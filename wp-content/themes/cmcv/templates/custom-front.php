869c0b1ebe23ed324cbbbf17e98787c9cda55ced
connyz <connymartinsson@yahoo.se>
2014-11-07 11:33:39 +0100

    Changes in:
    1. base.css
    2. responsive.css
    3. custom-front-php

--- wp-content/themes/cmcv/templates/custom-front.php
+++ wp-content/themes/cmcv/templates/custom-front.php
@@ -34,45 +34,53 @@ get_header();
 		</div>
 		<!-- Info custom fields -->
 		<div class="custom_info">
-			<p class="custom_field_p"><?php _e("Available for:", "cmcv")?></p>
-			<?php $frontpage_text = get_field( "availability" );
-			if( $frontpage_text ){ ?>
-				<p><?php the_field( "availability" ); ?></p>
-			<?php }else{
-				echo ("<p>No text available.</p>");
-			} ?>
-			<p class="custom_field_p"><?php _e("Cellphone:", "cmcv")?></p>
-			<?php $frontpage_text = get_field( "cellphone" );
-			if( $frontpage_text ){ ?>
-				<p><?php the_field( "cellphone" ); ?></p>
-			<?php }else{
-				echo ("<p>No text available.</p>");
-			} ?>
-			<p class="custom_field_p"><?php _e("E-mail:", "cmcv")?></p>
-			<?php $frontpage_text = get_field( "email" );
-			if( $frontpage_text ){ ?>
-				<p><?php the_field( "email" ); ?></p>
-			<?php }else{
-				echo ("<p>No text available.</p>");
-			} ?>
+			<div class="custom_info_section">
+				<p class="custom_field_p"><?php _e("Available for:", "cmcv")?></p>
+				<?php $frontpage_text = get_field( "availability" );
+				if( $frontpage_text ){ ?>
+					<p><?php the_field( "availability" ); ?></p>
+				<?php }else{
+					echo ("<p>No text available.</p>");
+				} ?>
+			</div>
+			<div class="custom_info_section">
+				<p class="custom_field_p"><?php _e("Cellphone:", "cmcv")?></p>
+				<?php $frontpage_text = get_field( "cellphone" );
+				if( $frontpage_text ){ ?>
+					<p><?php the_field( "cellphone" ); ?></p>
+				<?php }else{
+					echo ("<p>No text available.</p>");
+				} ?>
+			</div>
+			<div class="custom_info_section">
+				<p class="custom_field_p"><?php _e("E-mail:", "cmcv")?></p>
+				<?php $frontpage_text = get_field( "email" );
+				if( $frontpage_text ){ ?>
+					<p><?php the_field( "email" ); ?></p>
+				<?php }else{
+					echo ("<p>No text available.</p>");
+				} ?>
+			</div>
 			<!-- Social links -->
-			<p class="custom_field_p"><?php _e("Social links:", "cmcv")?></p>
-			<?php $front_fb_link = get_field( "facebook_link" );
-			if( $front_fb_link ){ ?>
-				<a class="facebook_link" href=<?php echo (the_field( "facebook_link" )) ?>></a>
-			<?php } ?>
-			<?php $front_tw_link = get_field( "twitter_link" );
-			if( $front_tw_link ){ ?>
-				<a class="twitter_link" href=<?php echo (the_field( "twitter_link" )) ?>></a>
-			<?php } ?>
-			<?php $front_li_link = get_field( "linkedin_link" );
-			if( $front_li_link ){ ?>
-				<a class="linkedin_link" href=<?php echo (the_field( "linkedin_link" )) ?>></a>
-			<?php } ?>
-			<?php $front_gp_link = get_field( "google+_link" );
-			if( $front_gp_link ){ ?>
-				<a class="googleplus_link" href=<?php echo (the_field( "google+_link" )) ?>></a>
-			<?php } ?>
+			<div class="custom_info_section">
+				<p class="custom_field_p"><?php _e("Social links:", "cmcv")?></p>
+				<?php $front_fb_link = get_field( "facebook_link" );
+				if( $front_fb_link ){ ?>
+					<a class="facebook_link" href=<?php echo (the_field( "facebook_link" )) ?>></a>
+				<?php } ?>
+				<?php $front_tw_link = get_field( "twitter_link" );
+				if( $front_tw_link ){ ?>
+					<a class="twitter_link" href=<?php echo (the_field( "twitter_link" )) ?>></a>
+				<?php } ?>
+				<?php $front_li_link = get_field( "linkedin_link" );
+				if( $front_li_link ){ ?>
+					<a class="linkedin_link" href=<?php echo (the_field( "linkedin_link" )) ?>></a>
+				<?php } ?>
+				<?php $front_gp_link = get_field( "google+_link" );
+				if( $front_gp_link ){ ?>
+					<a class="googleplus_link" href=<?php echo (the_field( "google+_link" )) ?>></a>
+				<?php } ?>
+			<div>
 		</div>
 
 		<?php endwhile; // end of the loop. ?>
